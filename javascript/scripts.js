d4ba365c0e10ec664d1697690acc49702e72992e
JD Arthur <ruhtradj@gmail.com>
2019-01-19 11:20:24 -0500

    refactored a bit of HTTP call code and documented functions more

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,64 +1,82 @@
-function reqListener () {
-    let data = JSON.parse(this.responseText)
-    create_table(data)
-}
+/*
+scripts for the All Stocks page
 
-function initialize() {
-    //console.log("test")
-    let oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    oReq.open("GET", "/api/stocks");
-    oReq.send();
+Functionality:
+    - Load all stocks from the database
+    - Edit information (date founded, name, exchange, active/inactive, etc.)
+    - Save to database
 
-    get_sectors()
-    get_exchanges()
-}
+Todo:
+    - Link to other pages (e.g. /daily?symbol=aapl)
+    - tabbed view to navigate between pages
 
+author: jdarthur
+date: 19 Jan 2019
+*/
+
+//These are used to fill dropdowns when you edit a stock.
 var dropdowns = {
     "sector" : [],
     "exchange" : []
 }
 
-function sectListener () {
+function reqListener () {
+    /*
+    parse stock data and use it to create a table
+    */
     let data = JSON.parse(this.responseText)
-    for (let i = 0; i < data.length; i ++){
+    create_table(data)
+}
+
+function initialize() {
+    /*
+    run functions we need to set up this page
+    */
+    let oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener);
+    oReq.open("GET", "/api/stocks");
+    oReq.send();
+
+    get_all("sector")
+    get_all("exchange")
+}
+
+function get_all_listener() {
+    /*
+    get list data via REST (all exchanges, all sectors, etc)
+    */
+    const type = this.datatype
+    let data = JSON.parse(this.responseText)
+        for (let i = 0; i < data.length; i ++){
         const name = data[i]['name']
-        dropdowns.sector.push(name)
+        dropdowns[type].push(name)
     }
 }
 
-function get_sectors() {
-    //console.log("test")
-    let oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", sectListener);
-    oReq.open("GET", "/api/sectors");
-    oReq.send();
-}
+function get_all(type) {
+    let oReq = new XMLHttpRequest()
+    oReq.addEventListener("load", get_all_listener)
+    oReq.open("GET", "/api/" + type + "s")
+    oReq.send()
 
-function exchListener () {
-    let data = JSON.parse(this.responseText)
-    for (let i = 0; i < data.length; i ++){
-        const name = data[i]['name']
-        dropdowns.exchange.push(name)
-    }
-    console.log(dropdowns.exchange)
-}
-
-function get_exchanges() {
-    //console.log("test")
-    let oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", exchListener);
-    oReq.open("GET", "/api/exchanges");
-    oReq.send();
+    //use this field to know where we save data in the listener
+    oReq.datatype = type
 }
 
 function create_table(data) {
+    /*
+    Create a table with stock data
+    */
+
+    //create the table itself
     let div = document.getElementById("main_div")
     let table = document.createElement("table")
     table.setAttribute("class", "tab")
     table.setAttribute("id", "stocks_table")
-    headers = Object.keys(data[0])
 
+
+    //create headers
+    headers = Object.keys(data[0])
     const thead = table.createTHead()
     const row = thead.insertRow()
     row.setAttribute("class", "tabheader")
@@ -68,6 +86,7 @@ function create_table(data) {
         cell.innerHTML = headers[i]
     }
 
+    //create data rows
     const tbody = table.createTBody()
     for (let i = 0; i < data.length; i++) {
         const row = tbody.insertRow()
@@ -80,6 +99,7 @@ function create_table(data) {
         }
     }
 
+    //create "New Row" button
     const new_row = tbody.insertRow(tbody.rows.length)
     const button_cell = new_row.insertCell()
     const button = document.createElement("button")
@@ -91,6 +111,9 @@ function create_table(data) {
 }
 
 function add_row(element) {
+    /*
+    pop a new row into the table
+    */
 
     const row = element.parentNode.parentNode
     row.setAttribute("new", "true")
@@ -101,27 +124,28 @@ function add_row(element) {
         cell.setAttribute("onclick", "make_editable(this)")
     }
     row.cells[0].innerHTML = ""
-
     make_editable(row.cells[0])
-
 }
 
 function make_editable(cell) {
     /*
     make a row editable i.e. change text data into input boxes
+
+    columns like `exchange` & `sector` will be turned into a dropdown, rather than
+    a free text input.
     */
 
     const row = cell.parentNode
     const table = row.parentNode.parentNode
     const thead_row = table.tHead.rows[0]
 
-
+     //only do all this if this row is not already selected
     if ((row.getAttribute("active") != "true")) {
-        for (let i = 0; i < row.cells.length; i++) {
 
+        //run through all rows and turn them into inputs
+        for (let i = 0; i < row.cells.length; i++) {
             let text_input = null
             const cell = row.cells[i]
-
             if (thead_row.cells[i].innerHTML == "sector") {
                 text_input = create_dropdown(cell, "sector")
             }
@@ -130,7 +154,6 @@ function make_editable(cell) {
             }
 
             else {
-
                 text_input = document.createElement("input")
                 text_input.setAttribute("size", 10)
                 text_input.value = cell.innerHTML
@@ -140,7 +163,8 @@ function make_editable(cell) {
             cell.appendChild(text_input)
 
         }
-        //add plus button
+
+        //add "Save" button
         const button_cell = row.insertCell()
         const button = document.createElement("button")
         button.setAttribute("onclick", "save_row(this)")
@@ -170,6 +194,9 @@ function create_dropdown(cell, type) {
 }
 
 function save_row(button) {
+    /*
+    Save a row to the database
+    */
     const row = button.parentNode.parentNode
     let post = false
     if (row.getAttribute("new") == "true") {
@@ -177,17 +204,17 @@ function save_row(button) {
         row.setAttribute("new", "false")
     }
 
-    //add plus button
+    //remove plus button
     button.remove()
     row.deleteCell(row.cells.length - 1)
     row.setAttribute("active", false)
 
+    //chaange all inputs back into text
     for (let i = 0; i < row.cells.length; i++) {
         const cell = row.cells[i]
         child = cell.childNodes[0]
         cell.innerHTML = child.value
     }
-
     data = row2dict(row)
     save_db(data, post)
 }
